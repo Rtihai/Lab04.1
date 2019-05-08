5c7315a60b653cd04e5e7fe5f572a2dbd343d684
Rtihai <rtihai@gmail.com>
2019-05-07 21:39:32 -0700

    Lab 04.2

--- app/src/main/java/com/roman/tihai/droidcafe/OrderActivity.java
+++ app/src/main/java/com/roman/tihai/droidcafe/OrderActivity.java
@@ -3,7 +3,13 @@ package com.roman.tihai.droidcafe;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.RadioButton;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class OrderActivity extends AppCompatActivity {
 
@@ -15,9 +21,67 @@ public class OrderActivity extends AppCompatActivity {
         String message = "Order: " + intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
         TextView textView = findViewById(R.id.order_textview);
         textView.setText(message);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.labels_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        Spinner spinner = findViewById(R.id.label_spinner);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                displayToast(parent.getItemAtPosition(position).toString());
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if (position == 0) {
+                    displayToast("Home Selected");
+                } else if (position == 1) {
+                    displayToast("Work Selected");
+                } else if (position == 2) {
+                    displayToast("Mobile Selected");
+                } else if (position == 3) {
+                    displayToast("Other selected");
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
     }
 
 
 
+    private void displayToast(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+    }
 
+    public void onRadioButtonClicked(View view) {
+        RadioButton selected = (RadioButton) view;
+        // check if the button is checked
+        boolean checked = selected.isChecked();
+        if (selected.getId() == R.id.sameday) {
+            if (checked) {
+                displayToast("Same day service selected");
+            }
+        } else if (selected.getId() == R.id.nextday) {
+            if (checked) {
+                displayToast("Next day service selected");
+            }
+        } else if (selected.getId() == R.id.pickup) {
+                if (checked) {
+                    displayToast("Pick up service selected");
+                }
+        } else {
+        }
+    }
 }
